9aa2240cfa243e54d1976e444c36eaf46ae845b2
kulik <55189-guilhermekulik@users.noreply.drupalcode.org>
2023-08-11 19:28:14 -0300

    Adicionando perfil

--- app/views/perfil.php
+++ app/views/perfil.php
@@ -6,12 +6,14 @@ if (!isset($_SESSION['user'])) {
   throw new Exception('Acesso restrito.');
 }
 
+$user = $_SESSION['user']; 
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard</title>
+    <title>Perfil</title>
     <link rel="stylesheet" href="/public/css/dashboard.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
@@ -24,7 +26,7 @@ if (!isset($_SESSION['user'])) {
                 <h2>ADMIN</h2>
             </div>
             <ul class="sidebar-menu">
-            <li><a href="/app/views/dashboard.php">Dashboard</a></li>
+                <li><a href="/app/views/dashboard.php">Dashboard</a></li>
                 <li><a href="/app/views/perfil.php">Perfil</a></li>
                 <li><a href="#">Pontos</a></li>
                 <li><a href="#">Clientes</a></li>
@@ -34,7 +36,14 @@ if (!isset($_SESSION['user'])) {
         </div>
         <!-- Conteúdo -->
         <div class="content">
-            <h2> Olá <?php echo $_SESSION['user']['name'];?>, bem vindo!
+            <p>Nome: <?php echo $user['name'];?></p>
+            <p>E-mail: <?php echo $user['email'];?></p>
+            <p>Celular: <?php echo $user['phone'];?></p>
+            <p>Endereço: <?php echo $user['address'];?></p>
+            <p>Cep: <?php echo $user['cep'];?></p>
+            <p>Bairro: <?php echo $user['bairro'];?></p>
+            <p>Número: <?php echo $user['houseNumber'];?></p>
+            <p>Nível: <?php echo $user['userType'];?></p>
         </div>
     </div>
 </body>
