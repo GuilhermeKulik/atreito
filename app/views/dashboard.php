85406f282a8ab6d4b3cb2bee01b14a3f084cd2fa
kulik <55189-guilhermekulik@users.noreply.drupalcode.org>
2023-08-11 15:05:06 -0300

    Add dashboard page

---
+++ app/views/dashboard.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Painel de Administração</title>
+    <link rel="stylesheet" href="/public/css/dashboard.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="dashboard-wrapper">
+        <!-- Barra Lateral -->
+        <div class="sidebar">
+            <div class="sidebar-header">
+                <h2>BC NATURAL</h2>
+            </div>
+            <ul class="sidebar-menu">
+                <li><a href="#">Perfil</a></li>
+                <li><a href="#">Pontos</a></li>
+                <li><a href="#">Usuários</a></li>
+                <li><a href="#">Configurações</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+        <!-- Conteúdo -->
+        <div class="content">
+            <!-- Aqui você pode adicionar o conteúdo do painel de administração -->
+            <!-- Por enquanto, deixaremos em branco -->
+        </div>
+    </div>
+</body>
+</html>
