40e39ef57510b116c27a81879ef5ef3b05d13f05
Guilherme Kulik <guilhermekuliK@gmail.com>
2023-09-07 00:36:43 -0300

    Componentes

---
+++ app/Core/Component/Sidebar.php
@@ -0,0 +1,12 @@
+<body>
+    <div class="dashboard-wrapper d-flex">
+        <!-- Barra Lateral -->
+        <div class="sidebar">
+            <ul class="sidebar-menu">
+                <li><a href="/dashboard">Dashboard</a></li>
+                <li><a href="/usuarios">Perfil</a></li>
+                <li><a href="/cadastro-usuario">Cadastro</a></li>
+                <li><a href="/configuracoes">Configurações</a></li>
+                <li><a href="/logout">Logout</a></li>
+            </ul>
+        </div>
